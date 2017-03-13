6d0565777796f53081f099451cc5c8420ece0813
Andrea Frittella <andreafrittella@local.dev>
2017-03-13 09:45:04 +0100

    Removed webpack.mix.js

--- webpack.mix.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by andreafrittella on 24/02/17.
- */
-
-const { mix } = require('laravel-mix');
-
-mix.copy('vendor/bower_components/jquery-form/dist/jquery.form.min.js', 'src/public/js/');
