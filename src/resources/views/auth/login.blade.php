0434b7e2d77f49c85b932d5edfe7ee418fa8c28f
Shem Pasamba <shemgp@users.noreply.github.com>
2017-10-18 13:52:20 +0800

    Make it work in subfolder

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@ login-page
 @section('content')
     <div class="login-box">
         <div class="login-logo">
-            <a href="/">{{config('app.name') }}</a>
+            <a href="{{config('app.url')}}">{{config('app.name') }}</a>
         </div>
         <!-- /.login-logo -->
 
